d708213e8be7f26bfdcb1969828386d62aaed4a5
KeDataLab <Kedatalab@gmail.com>
2026-03-11 20:06:31 +0300

    SQL Analysis

---
+++ project files/product analysis.sql
@@ -0,0 +1,13 @@
+/*
+Product Performance Analysis
+What are top performing products?
+*/
+SELECT 
+ p.product_name,
+ SUM(f.sales_amount) AS Revenue,
+ SUM(f.quantity) AS Units_Sold
+FROM gold.fact_sales f
+JOIN gold.dim_products p 
+   ON f.product_key = p.product_key
+GROUP BY p.product_name
+ORDER BY Revenue DESC;
